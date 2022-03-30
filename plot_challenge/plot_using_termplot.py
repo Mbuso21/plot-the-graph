febd3c834be8723b5a517c5ee03406cfe750751f
mmehloma <mmehloma@student.wethinkcode.co.za>
2022-03-30 19:19:26 +0200

    Cleaned up the code and it runs perfectly

--- plot_challenge/plot_using_termplot.py
+++ plot_challenge/plot_using_termplot.py
@@ -5,50 +5,6 @@ import requests as req
 from datetime import date
 import json
 
-def work():
-    y = []
-    x = []
-
-    api_data = {
-        "01-01-2022":300,
-        "02-01-2022":500,
-        "03-01-2022":700,
-        "04-01-2022":1300,
-        "05-01-2022":2000,
-        "06-01-2022":3000,
-        "07-01-2022":3500,
-        "08-01-2022":4000,
-        "09-01-2022":4500,
-        "10-01-2022":5000,
-        "11-01-2022":20000,
-        "12-01-2022":35000,
-        "13-01-2022":46000,
-        "14-01-2022":70000,
-        "15-01-2022":90000
-    }
-
-    response = req.get("http://sam-user-activity.eu-west-1.elasticbeanstalk.com/")
-
-    response_text = response.text
-    json_data = json.loads(response_text)
-    print(json_data)
-    print(type(json_data))
-    print(type(response_text))
-
-
-    print(datetime.strptime('01-01-2022', '%d-%m-%Y'))
-
-    for k in json_data:
-        y.append(datetime.strptime(k, '%d-%m-%Y'))
-        x.append(api_data[k])
-
-    print(y)
-    print(x)
-
-    mpl.plot_date(y, x, xdate=True, ydate=False)
-    mpl.show()
-    
-
 
 def getDataFromAPI(api_url):
     """
@@ -67,7 +23,7 @@ def convertDataIntoLists(data):
     y = []
     for k in data:
         y.append(datetime.strptime(k, '%d-%m-%Y'))
-        x.append(api_data[k])
+        x.append(data[k])
 
     return x, y
     
@@ -76,13 +32,15 @@ def plotGraph(x_axis, y_axis):
     Takes xaxis(list) a list of x co-ordinates
     """
 
-    mpl.plot_date(y, x, xdate=True, ydate=False)
+    mpl.plot_date(y_axis, x_axis, xdate=True, ydate=False)
     mpl.show()
 
 
 def run():
-    print(hello)
+    data = getDataFromAPI("http://sam-user-activity.eu-west-1.elasticbeanstalk.com/")
+    x_axis, y_axis = convertDataIntoLists(data)
+    plotGraph(x_axis, y_axis)
 
 
 if __name__ == "__main__":
-    work()
+    run()
